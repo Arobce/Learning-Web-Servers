655344a5d5cc9b5f91f29fb3cd6ee33e7f475f91
Roshan Chapagain <arobce@gmail.com>
2019-09-27 12:27:23 +0545

    Introduced Partials

--- src/app.js
+++ src/app.js
@@ -2,14 +2,23 @@ const path = require("path");
 
 const express = require("express");
 const app = express();
+const hbs = require('hbs');
 
-const publicPath = path.join(__dirname, "../public");
+const configPaths = {
+    publicPath: path.join(__dirname, "../public"),
+    viewsPath: path.join(__dirname,"../templates/views"),
+    partialsPath: path.join(__dirname,"../templates/partials")
+}
 
 //Set Engine
 app.set('view engine', 'hbs');
+//Set views Path
+app.set('views',configPaths.viewsPath);
+//Register Partials
+hbs.registerPartials(configPaths.partialsPath);
 
 //Static Files
-app.use(express.static(publicPath))
+app.use(express.static(configPaths.publicPath))
 
 //Routes
 app.get('', (req, res) => {
